5765ef4833973d8db1cd4282e99a7d28b60d7440
othmane <othman-19@hotmail.fr>
2019-06-30 20:58:00 +0100

    Add actions display flashes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,12 +15,15 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         @article.save
+        flash.notice = "Article '#{@article.title}' Created!"
         redirect_to article_path(@article)
     end
 
     def destroy
       Article.find(params[:id]).destroy
+      flash.notice = "Article Deleted!"
       redirect_to articles_path
+     
     end
     
     def edit
@@ -30,6 +33,7 @@ class ArticlesController < ApplicationController
     def update
       @article = Article.find(params[:id])
       @article.update(article_params)
+      flash.notice = "Article '#{@article.title}' Updated!"
       redirect_to article_path(@article)
     end
 
